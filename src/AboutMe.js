d7c6d517a51523b96f1f0cf0afb5cd6abc27dd73
qixiaoling <x.qi@novi-education.nl>
2021-07-22 21:21:22 +0200

    Open Highlight

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import eric from './Assets/eric_IMG_0897.jpg'
 import './AboutMe.css'
 
 function AboutMe() {
+    const [hightLightOpen, setHighLightOpen] = useState(false);
+
+    function renderClasses(){
+        let classes = 'high-light-list';
+        if(hightLightOpen){
+            classes += 'active';
+        }
+        return classes;
+    }
+
     return (
         <div className='about-me-container'>
             <div className='pop-up-container'>
                 <div className='img-container'>
                     <img src={eric} alt='Eric'/>
                 </div>
-                <button className='high-light-button'>
-                    Hightlights
+                <button className='high-light-button' onClick={()=>setHighLightOpen(!hightLightOpen)}>
+                    {hightLightOpen ? 'Close Highlights' : 'See Highlights'}
                 </button>
+                <ul className={renderClasses()}>
+                    <li className='high-light-item'>Software Engineering degree</li>
+                    <li className='high-light-item'>More Than 30 Years Experience</li>
+                    <li className='high-light-item'>Lean Six Sigma Master Black</li>
+
+                </ul>
             </div>
             <div className='text-container'>
                 <h2>Hi I am Eric van Doorn</h2>

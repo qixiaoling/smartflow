f6a86671d90ebc861145224443795544487ee5c2
qixiaoling <x.qi@novi-education.nl>
2021-07-22 10:33:40 +0200

    correction

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--card', 'btn--page']
+const STYLES = ['btn--primary', 'btn--outline']
 const SIZES = ['btn--medium', 'btn--large', 'btn--small']
 export const Button = ({
                            children,

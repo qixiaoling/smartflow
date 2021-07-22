ff0435f4e46000a098e5df9210b158d4dda827e5
qixiaoling <x.qi@novi-education.nl>
2021-07-22 13:30:18 +0200

    About me correction

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,12 +8,10 @@ function AboutMe() {
             <div className='pop-up-container'>
                 <div className='img-container'>
                     <img src={eric} alt='Eric'/>
-                    <div className='pop-up pop-up1'>1</div>
-                    <div className='pop-up pop-up2'>2</div>
-                    <div className='pop-up pop-up3'>3</div>
-                    <div className='pop-up pop-up4'>4</div>
-                    <div className='pop-up pop-up5'>5</div>
                 </div>
+                <button className='high-light-button'>
+                    Hightlights
+                </button>
             </div>
             <div className='text-container'>
                 <h2>Hi I am Eric van Doorn</h2>
@@ -29,15 +27,12 @@ function AboutMe() {
                 </p>
                 <br/>
                 <p>
-                    Fusce ac lorem sit amet elit fermentum tempor sed sit amet lacus. Nam sodales, tellus quis rhoncus
-                    commodo, est leo cursus dui, sed luctus lectus eros quis erat. Mauris scelerisque auctor pretium.
-                    Phasellus sagittis tempus diam vitae rhoncus. Cras ac dictum orci.
-                    Fusce fringilla placerat nunc, eget posuere eros varius vel. Donec non cursus mauris. Sed
-                    vestibulum orci velit, vitae fringilla ipsum elementum sit amet. Aliquam vulputate arcu at
-                    venenatis laoreet. Morbi egestas est tristique interdum ullamcorper. Sed dapibus
-                    eleifend erat, id interdum lorem molestie a. Nam fringilla metus at nisi bibendum, eu euismod diam
-                    varius. Aliquam porttitor porttitor maximus.
+                    Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
+                    vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
+                    ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
+                    Integer sed placerat mi.
                 </p>
+
 
             </div>
 

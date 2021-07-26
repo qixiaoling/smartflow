49c25fde16ffe3e456a638eda5afda54eb8a8644
qixiaoling <x.qi@novi-education.nl>
2021-07-26 13:39:04 +0200

    Footer

---
+++ src/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Footer.css'
+import {Link} from "react-router-dom";
+import {Button} from "./Button";
+
+function Footer() {
+    return(
+        <div className='footer-container'>
+            <Link to='/' className='footer-logo-container'>
+                SmartFlow<i className="fab fa-battle-net"></i>
+            </Link>
+            <br/>
+            <br/>
+            <div className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <Link to='/about-me'>About Me</Link>
+                        <Link to='/project'>Project</Link>
+
+                    </div>
+                    <div className='footer-link-items'>
+                        <Link to='/review'>Review</Link>
+                        <Link to='/contact'>Contact</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <Button
+                        className='btn'
+                        buttonStyle='btn--outline'
+                        buttonSize='btn--large'
+                        data-testid='test'
+                    >
+                        Get Started
+                    </Button>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import HeroSection from "./HeroSection";
 import Tile from "./Tile";
 import Clients from "./Clients";
+import Footer from "./Footer";
+
 function Home(){
     return(
         <>
             <HeroSection />
             <Tile/>
             <Clients />
+            <Footer />
         </>
     )
 }

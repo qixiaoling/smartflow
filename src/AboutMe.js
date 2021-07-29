0221cdd314daecd33fa00ff3697147e7d800eaf3
qixiaoling <x.qi@novi-education.nl>
2021-07-29 20:40:11 +0200

    about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -15,41 +15,41 @@ function AboutMe() {
 
     return (
         <div className='about-me-container'>
-            <div className='pop-up-container'>
-                <div className='img-container'>
-                    <img src={eric} alt='Eric'/>
+            <div className='about-me-wrapper'>
+                <div className='pop-up-container'>
+                    <div className='img-container'>
+                        <img src={eric} alt='Eric'/>
+                    </div>
+                    <button className='high-light-button' onClick={()=>setHighLightOpen(!hightLightOpen)}>
+                        {hightLightOpen ? 'Close Highlights' : 'See Highlights'}
+                    </button>
+                    <ul className={renderClasses()}>
+                        <li className='high-light-item'>Software Engineering degree</li>
+                        <li className='high-light-item'>More Than 30 Years Experience</li>
+                        <li className='high-light-item'>Lean Six Sigma Master Black Belt</li>
+
+                    </ul>
                 </div>
-                <button className='high-light-button' onClick={()=>setHighLightOpen(!hightLightOpen)}>
-                    {hightLightOpen ? 'Close Highlights' : 'See Highlights'}
-                </button>
-                <ul className={renderClasses()}>
-                    <li className='high-light-item'>Software Engineering degree</li>
-                    <li className='high-light-item'>More Than 30 Years Experience</li>
-                    <li className='high-light-item'>Lean Six Sigma Master Black Belt</li>
-
-                </ul>
-            </div>
-            <div className='text-container'>
-                <h2>Hi I am Eric van Doorn</h2>
-                <br/>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, justo at
-                    consequat sagittis, leo est tincidunt velit, eget consectetur neque turpis a ex. Mauris ut
-                    molestie nibh, eget facilisis risus. Proin sed mollis dolor. Morbi elementum massa nulla,
-                    eu rhoncus nisl vulputate et. Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
-                    vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
-                    ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
-                    Integer sed placerat mi. In ultrices turpis eget pretium gravida.
-                </p>
-                <br/>
-                <p>
-                    Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
-                    vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
-                    ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
-                    Integer sed placerat mi.
-                </p>
+                <div className='text-container'>
+                    <h2>Hi I am Eric van Doorn</h2>
+                    <br/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, justo at
+                        consequat sagittis, leo est tincidunt velit, eget consectetur neque turpis a ex. Mauris ut
+                        molestie nibh, eget facilisis risus. Proin sed mollis dolor. Morbi elementum massa nulla,
+                        eu rhoncus nisl vulputate et. Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
+                        vel, consequat viverra odio.
+                    </p>
+                    <br/>
+                    <p>
+                        Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
+                        vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
+                        ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
+                        Integer sed placerat mi.
+                    </p>
 
 
+                </div>
             </div>
 
         </div>

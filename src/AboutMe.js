5cf772fa962761ee1b77ffd6c2efebb78a9af1f5
qixiaoling <x.qi@novi-education.nl>
2021-08-02 10:39:27 +0200

    AboutMe.css re-written-3 (inner-container is not neccessary)

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -15,32 +15,34 @@ function AboutMe() {
 
     return (
         <div className='about-me-container'>
-            <div className='container container-left'>
-                <img src={eric} alt='Eric'/>
-                <button className='high-light-button' onClick={() => setHighLightOpen(!highLightOpen)}>
-                    {highLightOpen ? 'Close Highlights' : 'See Highlights'}
-                </button>
-                <ul className={renderClasses()}>
-                    <li className='high-light-item'>Software Engineering degree</li>
-                    <li className='high-light-item'>More Than 30 Years Experience</li>
-                    <li className='high-light-item'>Lean Six Sigma Master Black Belt</li>
-                </ul>
-            </div>
-            <div className='container container-right'>
-                <h2>Hi I am Eric van Doorn</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, justo at
-                    consequat sagittis, leo est tincidunt velit, eget consectetur neque turpis a ex. Mauris ut
-                    molestie nibh, eget facilisis risus. Proin sed mollis dolor. Morbi elementum massa nulla,
-                    eu rhoncus nisl vulputate et.
-                </p>
-                <p>
-                    Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
-                    vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
-                    ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
-                    Integer sed placerat mi.
-                </p>
-            </div>
+
+                <div className='container container-left'>
+                    <img src={eric} alt='Eric'/>
+                    <button className='high-light-button' onClick={() => setHighLightOpen(!highLightOpen)}>
+                        {highLightOpen ? 'Close Highlights' : 'See Highlights'}
+                    </button>
+                    <ul className={renderClasses()}>
+                        <li className='high-light-item'>Software Engineering degree</li>
+                        <li className='high-light-item'>More Than 30 Years Experience</li>
+                        <li className='high-light-item'>Lean Six Sigma Master Black Belt</li>
+                    </ul>
+                </div>
+                <div className='container container-right'>
+                    <h2>Hi I am Eric van Doorn</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, justo at
+                        consequat sagittis, leo est tincidunt velit, eget consectetur neque turpis a ex. Mauris ut
+                        molestie nibh, eget facilisis risus. Proin sed mollis dolor. Morbi elementum massa nulla,
+                        eu rhoncus nisl vulputate et.
+                    </p>
+                    <p>
+                        Ut aliquet laoreet gravida. Praesent erat sem, fringilla tempor est
+                        vel, consequat viverra odio. Ut sit amet tellus venenatis, volutpat est et, posuere neque. Donec
+                        ut consectetur est. Pellentesque semper tempor arcu quis imperdiet.
+                        Integer sed placerat mi.
+                    </p>
+                </div>
+
         </div>
     )
 }
